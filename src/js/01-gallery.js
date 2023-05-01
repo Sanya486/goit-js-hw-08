08e73fb83f5f26af71504d0bcdf0bae2081725d3
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-05-02 02:04:28 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
 // Change code below this line
 
 const refs = {
